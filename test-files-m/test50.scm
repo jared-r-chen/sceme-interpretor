e3caa1ff219ab8de6dbb7dd41dc4cefdaf00c5b3
jared-r-chen <89424574+jared-r-chen@users.noreply.github.com>
2021-12-06 15:48:59 -0600

    Add files via upload

---
+++ test-files-m/test50.scm
@@ -0,0 +1,18 @@
+(< 5 4)
+; (define x 12)
+; (set! x (+ 5 2))
+;  x
+
+; (let ((a 6))
+
+;   (begin (define x 3) (define y 4))
+;   (+ x y))
+  
+; (+ 1 (begin
+;          (printf "hi\n")
+;          2))
+    
+  ; ((begin (define x 3) (define y 4)) (+ x y))
+  
+    
+  ; (begin (define x 3) (define y 4) (+ x y))
